9a1420b05a16875d5a71011f14a732a613887f74
Tanner Harman <harmtan2@gmail.com>
2018-05-25 14:23:05 -0600

    Added Seed data for About, Contact, and WellnessProgram pages

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,43 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+3.times do |bio|
+  About.create!(
+    name: "Therapist #{bio + 1}", 
+    greeting: 'Get to Know Me!', 
+    about_me: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+    
+    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum', 
+    main_img: 'http://via.placeholder.com/600x400', 
+    thumb_img: 'http://via.placeholder.com/350x200'
+  )
+end
+
+puts "3 Biographies Created!"
+
+1.times do 
+  Program.create!(
+    title: "Wellness Program",
+    subtitle: 'Would you like to improve your…',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+    
+    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+    price: 15
+    )
+end
+puts "Wellness Program Created."
+
+1.times do
+  Contact.create!(
+    title: "Get in touch with Us!",
+    phone_number: "(307) 347-4001",
+    fax_number: "(307) 347-4001",
+    email: "worlandpt@outlook.com",
+    address: "120 South 17th St
+              Worland, WY 82401",
+    main_img: "http://via.placeholder.com/600x400"
+  )
+end
+
+puts "Contact Info Created!"
