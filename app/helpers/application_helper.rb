6ba6470a94e70ea735f24cdc289597e7179a28e7
Tanner Harman <harmtan2@gmail.com>
2018-05-30 21:40:39 -0600

    Refactored the login_helper to produce the register & login links as seperate divs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   def login_helper
     if current_user.is_a?(User)
-      link_to "Logout", destroy_user_session_path, method: :delete
+      content_tag(:div, "#{link_to "Logout", destroy_user_session_path, method: :delete}".html_safe)
     else
       (content_tag(:div, "#{link_to "Login", new_user_session_path}".html_safe)) +
       (content_tag(:div, "#{link_to "Register", new_user_registration_path}".html_safe))
