7fe07716352af8e52d56ab761314fa0bc0fb0c24
Tanner Harman <harmtan2@gmail.com>
2018-05-31 16:19:38 -0600

    removed login & register links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,7 @@
 module ApplicationHelper
 
   def login_helper
-    if current_user.is_a?(GuestUser)
-      (link_to("Login", new_user_session_path, class: "nav-link")) +
-      (link_to( "Register", new_user_registration_path, class: "nav-link"))
-    else
+    if !current_user.is_a?(GuestUser)
       link_to("Logout", destroy_user_session_path, method: :delete, class: "nav-link")
     end
   end
