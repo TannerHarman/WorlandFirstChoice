05a1f487735e8b821703c041be2b809e2044d4c1
Tanner Harman <harmtan2@gmail.com>
2018-05-31 08:52:02 -0600

    Refactored login_helper to be cleaner code

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,10 +2,10 @@ module ApplicationHelper
 
   def login_helper
     if current_user.is_a?(User)
-      content_tag(:div, "#{link_to "Logout", destroy_user_session_path, method: :delete}".html_safe)
+      content_tag(:div, link_to("Logout", destroy_user_session_path, method: :delete))
     else
-      (content_tag(:div, "#{link_to "Login", new_user_session_path}".html_safe)) +
-      (content_tag(:div, "#{link_to "Register", new_user_registration_path}".html_safe))
+      (content_tag(:div, link_to("Login", new_user_session_path))) +
+      (content_tag(:div, link_to( "Register", new_user_registration_path)))
     end
   end
 end
