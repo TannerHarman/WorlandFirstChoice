27808ce9c50fc6c7883d54e481e611cd78c82038
Tanner Harman <harmtan2@gmail.com>
2018-06-08 16:35:43 -0600

    Implemented CarrierWave for photo uploads

--- app/models/about.rb
+++ app/models/about.rb
@@ -4,6 +4,9 @@ class About < ApplicationRecord
 
   validates_presence_of :name, :greeting, :about_me
 
+  mount_uploader :thumb_img, ImageUploader
+  mount_uploader :main_img, ImageUploader
+
   def self.by_position
     order("position ASC")
   end
