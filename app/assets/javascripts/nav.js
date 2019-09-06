5430aa337b079216ba3f624556688867ac8bfaf1
Tanner Harman <harmtan2@gmail.com>
2019-09-05 21:37:05 -0600

    Fixed z-index bug for the learn more button

--- app/assets/javascripts/nav.js
+++ app/assets/javascripts/nav.js
@@ -1,6 +1,8 @@
 const menuButton = document.querySelector('.mobile-menu');
 const nav = document.querySelector('nav');
+const head = document.querySelector('header');
 
 menuButton.addEventListener('click', () => {
   nav.classList.toggle('show');
+  head.classList.toggle('show');
 });
