8454c34c835f3dbb6a64b90f7af359c9deded2af
Tanner Harman <harmtan2@gmail.com>
2019-09-07 00:26:27 -0600

    Fixed Nav & home button bug agian

--- app/assets/javascripts/nav.js
+++ app/assets/javascripts/nav.js
@@ -1,8 +1,6 @@
 const menuButton = document.querySelector('.mobile-menu');
 const nav = document.querySelector('nav');
-const homeButton = document.querySelector('.cta');
 
 menuButton.addEventListener('click', () => {
   nav.classList.toggle('show');
-  homeButton.classList.toggle('behind');
 });
