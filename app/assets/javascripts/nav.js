0f1b9ca26cf8dfccb7950f319d76eba0dd1db54b
Tanner Harman <harmtan2@gmail.com>
2019-09-07 00:01:59 -0600

    Fixed nav popup bug with home button

--- app/assets/javascripts/nav.js
+++ app/assets/javascripts/nav.js
@@ -1,8 +1,8 @@
 const menuButton = document.querySelector('.mobile-menu');
 const nav = document.querySelector('nav');
-const head = document.querySelector('header');
+const homeButton = document.querySelector('.cta');
 
 menuButton.addEventListener('click', () => {
   nav.classList.toggle('show');
-  head.classList.toggle('show');
+  homeButton.classList.toggle('behind');
 });
